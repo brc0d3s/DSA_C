63613533419309313421a17fefa1e91f537b14e4
brc0d3s <107584987+brc0d3s@users.noreply.github.com>
2023-11-14 11:10:37 +0300

    Update sort.c

--- Singly-LInked-List/sort.c
+++ Singly-LInked-List/sort.c
@@ -1,1 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void sortLinkedList(struct Node** head_ref) {
+    struct Node* current = *head_ref;
+    struct Node* index = NULL;
+    int temp;
+
+    if (head_ref == NULL) {
+        return;
+    } else {
+        while (current != NULL) {
+            index = current->next;
+            while (index != NULL) {
+                if (current->data > index->data) {
+                    temp = current->data;
+                    current->data = index->data;
+                    index->data = temp;
+                }
+                index = index->next;
+            }
+            current = current->next;
+        }
+    }
+}
+
+void insertNode(struct Node** head_ref, int data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+    } else {
+        struct Node* current = *head_ref;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
+}
+
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    // Insert nodes into the linked list
+    insertNode(&head, 5);
+    insertNode(&head, 2);
+    insertNode(&head, 8);
+    insertNode(&head, 1);
+    insertNode(&head, 4);
+
+    // Print the original linked list
+    printf("Original linked list: ");
+    printList(head);
+
+    // Sort the linked list
+    sortLinkedList(&head);
+
+    // Print the sorted linked list
+    printf("Sorted linked list: ");
+    printList(head);
+
+    // Free the memory
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return 0;
+}
 
